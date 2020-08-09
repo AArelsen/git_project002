5287a5d4c9d3a60d7a8fe61ce44d19ac33f93af9
Cevheren <68612514+Cevheren@users.noreply.github.com>
2020-08-09 18:08:50 +0200

    Update FirstClass.java

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -8,13 +8,13 @@ public class FirstClass {
 	System.out.println("This is second commit from local master");
 	System.out.println("This is the third commit from local master");
 	System.out.println("This is the first commit from remote repo");
-<<<<<<< HEAD
+
 	System.out.println("This is the forth commit from local master");
-=======
+
 		
 		
 	System.out.println("This is the second commit from remote master");
->>>>>>> 5b8c74b25007798ebe5987071010bcdb81a037da
+
 	}
 
 }
